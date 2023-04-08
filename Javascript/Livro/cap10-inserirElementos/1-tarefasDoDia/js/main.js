3cb6fad0824662ca435adaa1a9e8b89476d22728
Jheimys Bassani <jheimysbb@gmail.com>
2023-04-08 15:42:46 -0300

    :construction: Fazendo a logica para o botão selecionar

---
+++ Javascript/Livro/cap10-inserirElementos/1-tarefasDoDia/js/main.js
@@ -0,0 +1,50 @@
+;(function() {
+    //Variáveis global
+    const frm = document.querySelector('form')
+    const dvQuadrado = document.querySelector('#divQuadrado')
+
+    //Funções
+
+    //Eventos
+    frm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const tarefa = frm.inTarefa.value
+        
+        const h5 = document.createElement('h5')
+        const text = document.createTextNode(tarefa)
+        h5.appendChild(text)
+        dvQuadrado.appendChild(h5)
+
+        frm.inTarefa.value = ''
+        frm.inTarefa.focus()
+    })
+
+    frm.btSelecionar.addEventListener('click', () => {
+        
+        const tarefas = document.querySelectorAll('h5')
+
+        if(tarefas.length == 0) {
+            alert('Não há tarefas para selecionar')
+            return
+        }
+
+        let aux = -1
+
+        for(let i = 0; i < tarefas.length; i++) {
+
+            if(tarefas[i].className == 'tarefa-selecionada') {
+                tarefas[i].className = 'tarefa-normal'
+                aux = i
+                break
+            }
+        }
+
+        if(aux == tarefas.length - 1) {
+            aux = -1
+        }
+
+        tarefas[aux + 1].className = 'tarefa-selecionada'
+    })
+
+})()
