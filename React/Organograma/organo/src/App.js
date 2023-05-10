58f96d6e2ada69f38332181f05756b22b129469f
Jheimys Bassani <jheimysbb@gmail.com>
2023-05-10 17:01:19 +0100

    :recycle:Refatorando código duplicado

--- React/Organograma/organo/src/App.js
+++ React/Organograma/organo/src/App.js
@@ -5,6 +5,52 @@ import Time from './componentes/Time/Time';
 
 function App() {
 
+  const times = [
+
+    {
+      nome:'Programação',
+      corPrimaria:'#57C278',
+      corSecundaria:'#D9F7E9'
+    },
+
+    {
+      nome:'Front-End',
+      corPrimaria:'#82CFFA',
+      corSecundaria:'#E8F8FF'
+    },
+
+    {
+      nome:'Data Science',
+      corPrimaria:'#A6D157',
+      corSecundaria:'#FOF8E2'
+    },
+
+    {
+      nome:'Devops',
+      corPrimaria:'#EO6B69',
+      corSecundaria:'#FDE7E8'
+    },
+
+    {
+      nome:'UX e Design',
+      corPrimaria:'#DB6EBF',
+      corSecundaria:'#FAE9FS'
+    },
+
+    {
+      nome:'Mobile',
+      corPrimaria:'#FFBAO5',
+      corSecundaria:'#FFF5D9'
+    },
+
+    {
+      nome:'Inovação e Gestão',
+      corPrimaria:'#FF8A29',
+      corSecundaria:'#FFEEDF'
+    }
+ 
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionador = (colaborador) =>{
@@ -14,10 +60,8 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastro={colaborador => aoNovoColaboradorAdicionador(colaborador)} />
-      <Time nome="Programação" />
-      <Time nome="Frontend" />
-      <Time nome="Data Science" />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastro={colaborador => aoNovoColaboradorAdicionador(colaborador)} />
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
     </div>
   );
 }
