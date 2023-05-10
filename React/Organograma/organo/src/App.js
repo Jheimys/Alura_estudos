aba78be4051aaf3ac079277d40304b4cdb3db746
Jheimys Bassani <jheimysbb@gmail.com>
2023-05-10 08:32:19 +0100

    :construction: Criando o componente Time

--- React/Organograma/organo/src/App.js
+++ React/Organograma/organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario/Formulario';
+import Time from './componentes/Time/Time';
 
 function App() {
 
@@ -14,6 +15,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastro={colaborador => aoNovoColaboradorAdicionador(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Frontend" />
+      <Time nome="Data Science" />
     </div>
   );
 }
