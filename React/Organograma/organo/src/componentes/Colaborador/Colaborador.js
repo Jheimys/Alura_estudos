1e2ba2548c4c526802f4f2e7c4c089d9357475f6
Jheimys Bassani <jheimysbb@gmail.com>
2023-05-10 17:27:44 +0100

    :construction: Criando o componente de Colaboradores

---
+++ React/Organograma/organo/src/componentes/Colaborador/Colaborador.js
@@ -0,0 +1,18 @@
+import"./Colaborador.css"
+
+const Colaborador = () => {
+    return(
+        <div className="colaborador">
+            <div className="cabecalho">
+                <img src="https://github.com/jheimys.png"  alt="foto" />
+            </div>
+
+            <div className="rodape">
+                <h4>Jheimys</h4>
+                <h5>Aluno</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
