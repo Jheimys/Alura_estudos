f80816c7975c4af0f3a3d7985c541c70d0086c70
Jheimys Bassani <jheimysbb@gmail.com>
2023-05-16 06:23:08 +0100

    Implementando assinatura

---
+++ Nodejs/Criptografia_e_tokens/assinatura/assinatura.js
@@ -0,0 +1,34 @@
+import {generateKeyPairSync, createSign, createVerify} from 'crypto'
+
+const {privateKey, publicKey } = generateKeyPairSync('rsa',
+    {
+        modulusLength: 2048,
+
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        },
+    }
+)
+
+let dados = 'Essa string vai ser assinada!'
+
+//Assinatura
+const assinador = createSign('rsa-sha256')
+assinador.update(dados)
+
+const assinatura = assinador.sign(privateKey, 'hex')
+
+console.log(assinatura)
+
+//Envio desse documento -------documento, assinatura e chave pública
+const verificador = createVerify('rsa-sha256')
+verificador.update(dados)
+
+const EhVerificdo = verificador.verify(publicKey, assinatura, 'hex')
+
+console.log(EhVerificdo)
