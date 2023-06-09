c1c8b68e0f61a3d64e4b93d6f1e037944d6bc6e0
Jheimys Bassani <jheimysbb@gmail.com>
2023-06-09 12:48:40 +0100

    usando o grid

--- React/Tiago/my-app/src/pages/Customers.js
+++ React/Tiago/my-app/src/pages/Customers.js
@@ -1,12 +1,29 @@
 import { useEffect,  useState } from "react"
 import axios from "axios"
+import Grid from '@mui/material/Grid'
+// import { styled } from '@mui/material/styles'
+// import Paper from '@mui/material/Paper'
+
+
 import CustomersCard from "../componentes/CustomersCards"
+
+// const useStyles = styled(Paper)(({ theme }) => ({
+//      backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//      ...theme.typography.body2,
+//     padding: theme.spacing(1),
+//     textAlign: 'center',
+//      color: theme.palette.text.secondary,
+//      card: {
+//         paddind: theme.spacing(2),
+//      }
+//   }));
+
 
 const Customers = () => {
 
-    const [customers, setCustomers] = useState([])
+    // const classes = useStyles()
 
-    console.log(customers)
+    const [customers, setCustomers] = useState([])
 
     useEffect(() => {
         axios.get('https://reqres.in/api/users')
@@ -19,16 +36,22 @@ const Customers = () => {
     return(
         <>
             <h1>Customers</h1>
-            {
-                customers.map(item => (
-                    <CustomersCard 
-                        name={item.first_name}
-                        lastname={item.last_name}
-                        email={item.email}
-                        avatar={item.avatar}
-                    />
-                ))
-            }
+                <Grid container >
+                    {
+                        customers.map(item => (
+                            <Grid item xs={12} md={4} sx={{padding:'2px'}}>
+                                <CustomersCard 
+                                    name={item.first_name}
+                                    lastname={item.last_name}
+                                    email={item.email}
+                                    avatar={item.avatar}
+                                />
+                            </Grid>
+                        ))
+                    }
+
+                </Grid>
+           
         </>
     )
 }
