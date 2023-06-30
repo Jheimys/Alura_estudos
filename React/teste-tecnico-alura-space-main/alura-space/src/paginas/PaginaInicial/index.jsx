5719997cfc4ef1999e20199ee59d5ebb81296e39
Jheimys Bassani <jheimysbb@gmail.com>
2023-06-30 15:30:02 +0100

    Criando o componente de Menu

--- React/teste-tecnico-alura-space-main/alura-space/src/paginas/PaginaInicial/index.jsx
+++ React/teste-tecnico-alura-space-main/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,25 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+
+import styles from "./PaginaInicial.module.scss"
+
+import banner from"../../assets/banner.png"
+
 
 export default function PaginaInicial() {
     return(
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="imagem da terra vista do espaço" />
+                    </div>
+                </section>
+
+            </main>
+        </>
     )
 }
