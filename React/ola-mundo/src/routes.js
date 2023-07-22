da73fa87facf8d3cc6ffdf3c660744fd50e4fad4
Jheimys Bassani <jheimysbb@gmail.com>
2023-07-22 15:52:35 +0100

    :construction: Criando novas rotas

---
+++ React/ola-mundo/src/routes.js
@@ -0,0 +1,27 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Inicio from './paginas/Inicio'
+import SobreMim from './paginas/SobreMim'
+import Menu from './componentes/Menu';
+import Rodape from 'componentes/Rodape';
+import PaginaPadrao from 'componentes/PaginaPadrao';
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+
+        {/* Rotas anihadas */}
+        <Route path='/' element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path='sobremim' element={<SobreMim />} />
+        </Route>
+
+        <Route path='*' element={<div>Pagina não encontrada!</div>} />
+      </Routes>
+      <Rodape />
+    </BrowserRouter> 
+  );
+}
+
+export default AppRoutes;
