cc22d848520e18e8db199ed9a565a7d732ca978c
Jheimys Bassani <jheimysbb@gmail.com>
2024-01-07 19:20:31 +0000

    learn simple types

---
+++ typescript_documnetacao/2-tiposPrimitivos/build/primitivos.js
@@ -0,0 +1,8 @@
+"use strict";
+/* -------------- Type Assignment ----------------------*/
+// (1)- Explicit Type
+let firstName = "Dylan";
+console.log(typeof firstName);
+//(2) -Implicit Type
+let otherFirstName = "Bob";
+console.log(typeof otherFirstName);
---
+++ typescript_documnetacao/2-tiposPrimitivos/src/primitivos.ts
@@ -0,0 +1,10 @@
+/* -------------- Type Assignment ----------------------*/
+
+// (1)- Explicit Type
+let firstName: string = "Dylan";
+console.log(typeof firstName);
+
+//(2) -Implicit Type
+
+let otherFirstName = "Bob";
+console.log( typeof otherFirstName);
