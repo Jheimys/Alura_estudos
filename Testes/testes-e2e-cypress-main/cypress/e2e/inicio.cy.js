34325950f7d865b07502524a9283cd7bc42cdfa4
Jheimys Bassani <jheimysbb@gmail.com>
2024-02-17 17:46:18 +0000

    testes na pagina principal

--- Testes/testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ Testes/testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -1,10 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Página inicial', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/');
   });
 
-  it('Deve renderizar corretamente o texto da seção de vantagens', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('h2').contains('Vantagens do nosso banco:');
+  it('Deve renderizar o h1 com o texto correto', () => {
+    cy.getByData('titulo-principal').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
   });
 });
