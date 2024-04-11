a434a8cab94a77865385a6fa642bc182f4f6a294
Jheimys Bassani <jheimysbb@gmail.com>
2024-04-11 15:22:12 +0100

    Estudando next

--- Next/01-Nextjs-Course/pages/index.js
+++ Next/01-Nextjs-Course/pages/index.js
@@ -1,10 +1,36 @@
 import Link from "../components/Link";
 
+function GlobalStyle(){
+    return(
+        <style global jsx>{`
+            body{
+                font-family: sans-serif;
+            }
+        `}</style>
+    )
+}
+
+function Title({children, as}){
+    const Tag = as;
+    return(
+        <>
+            <Tag>
+                {children}
+            </Tag>
+                <style jsx>{`
+                    ${Tag}{
+                        color: red;
+                    }
+                `}</style>
+        </>
+    )
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura case - Home Page</h1>
+            <GlobalStyle />
+            <Title as="h1">Alura Cases - Home </Title>
             <Link href="/faq">
                 Ir para página do FAQ
             </Link>
