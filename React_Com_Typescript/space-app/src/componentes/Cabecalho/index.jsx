45899551c5e5a96a7dcb42a3257d46a27c54f264
Jheimys Bassani <jheimysbb@gmail.com>
2024-07-09 11:11:47 +0100

    Stylized input challenge

--- React_Com_Typescript/space-app/src/componentes/Cabecalho/index.jsx
+++ React_Com_Typescript/space-app/src/componentes/Cabecalho/index.jsx
@@ -1,23 +1,26 @@
+import CampoTexto from '../../CampoTexto'
 import styled from "styled-components"
 
 const HeaderEstilizado = styled.header`
     padding: 60px;
     display: flex;
     justify-content: space-between;
-    img{
-        width: 212px;
-        height: 65px;
-        top: 60px;
-        left: 24px;
-        gap: 0px;
-        opacity: 0px;
-    }
 `
+
+const Logo = styled.img`
+    width: 212px;
+    height: 65px;
+    top: 60px;
+    left: 24px;
+    gap: 0px;
+    opacity: 0px;
+` 
 
 const Cabecalho = () => {
     return(
         <HeaderEstilizado>
-            <img src="../public/imagens/logo.png" alt="logo"/>
+            <Logo src="../public/imagens/logo.png" alt="logo"/>
+            <CampoTexto />
         </HeaderEstilizado>
     )
 }
