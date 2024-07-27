91827594825bcbc733fe91a589531967a96996a9
Jheimys Bassani <jheimysbb@gmail.com>
2024-07-27 15:25:56 +0100

    tabuleiro

---
+++ udemy/Next.js/secao_2-revisaoReact/exercicios/src/pages/tabuleiro/tabuleiro1.jsx
@@ -0,0 +1,43 @@
+
+import styles from'./tabuleiro1.module.css'
+
+const quadradoBranco = <div className={styles.quadradoBranco}></div>
+const quadradoPreto = <div className={styles.quadradoPreto}></div>
+
+let linhasTabuleiro = []
+
+const geraLinha = (iniciarComBranco) => {
+    let linha = []
+
+    for(let  i = 0; i < 8; i++){
+        if(i % 2 === 0){
+            linha.push(iniciarComBranco ? quadradoBranco : quadradoPreto)
+        } else {
+            linha.push(iniciarComBranco ? quadradoPreto : quadradoBranco)
+        }
+    }
+
+    return linha
+}
+
+
+
+for(let i = 0; i < 8; i++){
+    linhasTabuleiro.push(
+        <div>
+            {geraLinha(i % 2 === 0)}
+        </div>
+    )
+}
+
+const tabuleiro = () => {
+
+
+    return(
+        <div className={styles.tabuleiroContainer}>
+            {linhasTabuleiro}
+        </div>
+    )
+}
+
+export default tabuleiro
