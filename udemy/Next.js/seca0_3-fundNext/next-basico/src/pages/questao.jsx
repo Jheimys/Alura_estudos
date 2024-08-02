8ebfedaf1ed2e5052cc454f11d40932f9b3cb626
Jheimys Bassani <jheimysbb@gmail.com>
2024-08-02 14:42:55 +0100

    finishing secao 3

---
+++ udemy/Next.js/seca0_3-fundNext/next-basico/src/pages/questao.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+
+
+function questao() {
+
+    const [ questao, setQuestao ] = useState(null)
+
+    useEffect(() => {
+        
+        fetch('http://localhost:3000/api/questao/123')
+            .then(resp => resp.json())
+            .then(setQuestao)
+
+    }, [])
+
+
+    function renderizarRespostas(){
+
+        if(questao){
+            return questao.respostas.map((resposta, i) => {
+                return <li key={i}>{resposta}</li>
+            })
+        }
+
+        return false
+    }
+
+  return (
+    <div>
+        <h1>Questão</h1>
+
+        <div>
+            <span>{questao?.id} - {questao?.enunciado}</span>
+            <ul>
+                {renderizarRespostas()}
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default questao
