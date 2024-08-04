05e8d0660bdae5debfb2a436871c5c3f4d7bb25b
Jheimys Bassani <jheimysbb@gmail.com>
2024-08-04 19:52:57 +0100

    create door #1

--- udemy/Next.js/secao_04-PortaPremiada/portas/src/conponents/Porta.jsx
+++ udemy/Next.js/secao_04-PortaPremiada/portas/src/conponents/Porta.jsx
@@ -3,8 +3,12 @@ import styles from "../styles/Porta.module.css"
 
 const Porta = () => {
   return (
-    <div className={styles.porta}>
-        <div className={styles.numero}> 3 </div>
+    <div className={styles.area}>
+      <div className={styles.estrutura}>
+        <div className={styles.porta}>
+            <div className={styles.numero}> 3 </div>
+        </div>
+      </div>
     </div>
   )
 }
