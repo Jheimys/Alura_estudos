c09ae47da7f794a9291e2f440feca33dee581197
Jheimys Bassani <jheimysbb@gmail.com>
2024-08-07 09:01:25 +0100

    add solução dos exercícios

---
+++ udemy/python/secao3-tipos_de_dados/Exercicio1.py
@@ -0,0 +1,15 @@
+# 1. Faça um programa que leia um número inteiro e imprima-o.
+
+num = 43
+print(num)
+
+# 2.Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
+# 3. Faça um programa que recebe três valores e apresente a soma dos quadrados dos valores lidos.
+
+valor1 = int(input('Digite o primeiro valor'))
+valor2 = int(input('Digite o segundo valor'))
+valor3 = int(input('Digite o terceiro valor'))
+
+print(valor1 + valor2 + valor3)
+print(valor1**2 + valor2**2 + valor3**2)
+
