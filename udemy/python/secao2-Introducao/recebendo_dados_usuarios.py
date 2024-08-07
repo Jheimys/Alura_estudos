d081885d4a55b0830324e13e45f7ab08472f184c
Jheimys Bassani <jheimysbb@gmail.com>
2024-08-07 08:04:16 +0100

    recebendo dados do usuario

---
+++ udemy/python/secao2-Introducao/recebendo_dados_usuarios.py
@@ -0,0 +1,15 @@
+
+print("Qual o seu nome?")
+nome = input()
+
+print('Qual a sua idade?')
+idade = input()
+
+# Exemplo de print antigo
+# print('%s tem %s anos' % (nome, idade))
+
+# Segundo exemplo de print
+#print('{0} tem {1} anos' .format(nome, idade))
+
+# 3° forma de print
+print(f'{nome} tem {idade} anos')
