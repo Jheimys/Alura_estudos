55fb90c0dea867482235c483cf327ebd8e9d760e
Jheimys Bassani <jheimysbb@gmail.com>
2024-09-09 10:36:01 +0100

    Finalizando o projeto

--- udemy/Next.js/secao_04-PortaPremiada/portas/src/pages/index.jsx
+++ udemy/Next.js/secao_04-PortaPremiada/portas/src/pages/index.jsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
 import Cartao from '../conponents/Cartao'
 import styles from '../styles/Formulario.module.css'
+import EntradaNumerica from "../conponents/EntradaNumerica"
+import { useState } from 'react';
 
 export default function Formulario() {
+
+  const [qtdPortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
 
   return (
     <div className={styles.formulario}>
@@ -10,16 +15,26 @@ export default function Formulario() {
         <Cartao bgcolor="#c0392c">
           <h1> Monty Hall</h1>
         </Cartao>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica 
+            text='Qtde Portas?'
+            value={qtdPortas}
+            onChange={novaQtde => setQtdePortas(novaQtde)}
+          />
+        </Cartao>
       </div>
       <div>
-        <Cartao ></Cartao>
-
+        <Cartao >
+          <EntradaNumerica 
+              text='Porta com presente?'
+              value={comPresente}
+              onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)}
+            />
+        </Cartao>
         <Cartao bgcolor="#28a085">
-          <Link href={`/jogo/4/2`}>
+          <Link href={`/jogo/${qtdPortas}/${comPresente}`}>
             <h2 className={styles.link}>Iniciar</h2>
           </Link>
-          
         </Cartao>
       </div>
     </div>
