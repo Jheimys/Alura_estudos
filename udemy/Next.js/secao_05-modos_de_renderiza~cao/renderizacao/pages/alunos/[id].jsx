b5fc81c1fcdd25775c89ed9ba11bc29762e6076f
Jheimys Bassani <jheimysbb@gmail.com>
2024-09-18 09:18:33 +0100

    finalizando a secao

--- udemy/Next.js/secao_05-modos_de_renderiza~cao/renderizacao/pages/alunos/[id].jsx
+++ udemy/Next.js/secao_05-modos_de_renderiza~cao/renderizacao/pages/alunos/[id].jsx
@@ -1,24 +1,37 @@
-export function getStaticPaths(){
+export async function getStaticPaths(){
+    const res = await fetch(`http://localhost:3000/api/tutores`)
+    const ids = await res.json()
+
+    const paths = ids.map(id => (
+        {params: {id: `${id}`}}
+    ))
+
     return{
         fallback: false,
-        paths:[
-            {params: {id: '107'}},
-            {params: {id: '203'}},
-            {params: {id: '1345'}}
-        ]
+        paths   
     }
 }
 
-export function getStaticProps(){
+export async function getStaticProps({params}){
+    const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`)
+    const aluno = await resp.json()
     return{
-        props:{}
+        props:{
+            aluno
+        }
     }
 }
 
-export default function AlunoPorId(){
+export default function AlunoPorId(props){
+    const { aluno } = props
     return(
         <div>
             <h1>Detalhes do Aluno</h1>
+            <ul>
+                <li>{aluno.id}</li>
+                <li>{aluno.nome}</li>
+                <li>{aluno.email}</li>
+            </ul>
         </div>
     )
 }
