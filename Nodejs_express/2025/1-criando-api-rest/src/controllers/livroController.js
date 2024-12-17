12bd9955bba6ff7cd0d6fccce1aa9aa61fb8ff79
Jheimys Bassani <jheimysbb@gmail.com>
2024-12-17 09:44:54 +0000

    Creating CRUD

--- Nodejs_express/2025/1-criando-api-rest/src/controllers/livroController.js
+++ Nodejs_express/2025/1-criando-api-rest/src/controllers/livroController.js
@@ -3,16 +3,55 @@ import livro from "../models/livro.js"
 class LivroController {
 
     static async listarLivros(req, res) {
-        const listaLivros = await livro.find({})
-        res.status(200).json(listaLivros);
+        try {
+            const listaLivros = await livro.find({})
+            res.status(200).json(listaLivros);
+            
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na requisição`})
+        }
     }
 
-    static async cadastrarLivro(req, res) {
+    static async listarLivroPorId(req, res) {
         try {
-            const novoLivro = await livro.create(req.body)
-            res.status(201).json({message: `${erro.message}`, livro: novoLivro})
+            const id = req.params.id
+            const livroEncontrado = await livro.findById(id)
+            res.status(200).json(livroEncontrado);
+            
         } catch (erro) {
-            restart.status(500).json({message: `${erro.message} - Falha ao cadastrar livro`})
+            res.status(500).json({message: `${erro.message} - Falha na requisição do livro`})
+        }
+    }
+
+    static async cadastrarLivro (req, res) {
+        try {
+          const novoLivro = await livro.create(req.body);
+          res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+        } catch (erro) {
+          res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
+        }
+    }
+
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id
+            await livro.findByIdAndUpdate(id, req.body)
+            res.status(200).json({message: `Livro atualizado`});
+            
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na atualização`})
+        }
+    }
+
+    
+    static async excluirLivro(req, res) {
+        try {
+            const id = req.params.id
+            await livro.findByIdAndDelete(id)
+            res.status(200).json({message: `Livro excluido com sucesso`});
+            
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message}- Livro excluido com sucesso`})
         }
     }
 
