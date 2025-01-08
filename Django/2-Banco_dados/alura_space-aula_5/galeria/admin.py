93cdee5973861194714940476866b7f5eafe22d2
Jheimys Bassani <jheimysbb@gmail.com>
2025-01-08 09:56:50 +0000

    BD Config

--- Django/2-Banco_dados/alura_space-aula_5/galeria/admin.py
+++ Django/2-Banco_dados/alura_space-aula_5/galeria/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+# Personalizando o que quero mostrar no BD
+
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', )
+
 
 # Register your models here.
+admin.site.register(Fotografia, ListandoFotografias)
