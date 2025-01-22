61cd7373e800cabe005c648cc0f384e55963365f
Jheimys Bassani <jheimysbb@gmail.com>
2025-01-22 09:22:46 +0000

    Alert and massages

--- Django/3-Altencicacao_forms/alura-space/usuarios/views.py
+++ Django/3-Altencicacao_forms/alura-space/usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 
 def login(request):
@@ -22,8 +23,10 @@ def login(request):
 
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f'{nome} logado com sucesso!')
             return redirect('index')
         else:
+            messages.error(request, 'Erro ao efetuar login')
             return redirect('login')
 
     return render(request, 'usuarios/login.html', {"form": form})
@@ -38,6 +41,7 @@ def cadastro(request):
         if form.is_valid():
 
             if form['senha_1'].value() != form["senha_2"].value():
+                messages.error(request, 'Senhas não iguais')
                 return redirect('cadastro')
 
             nome = form['nome_cadastro'].value()
@@ -45,6 +49,7 @@ def cadastro(request):
             senha = form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, 'Usuário já existente')
                 return redirect('cadastro')
 
         usuario = User.objects.create_user(
@@ -54,6 +59,7 @@ def cadastro(request):
         )
 
         usuario.save()
+        messages.success(request, 'cadastro efetuado com sucesso!')
 
         return redirect('login')
 
