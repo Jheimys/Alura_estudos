9d1feee60e02605582d460fbd666154689bfcb32
Jheimys Bassani <jheimysbb@gmail.com>
2025-02-03 09:08:44 +0000

    Finishing the project

--- Django/3-Altencicacao_forms/alura-space/setup/settings.py
+++ Django/3-Altencicacao_forms/alura-space/setup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path, os
 from dotenv import load_dotenv
 
@@ -144,3 +145,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-jheimys-aluraestudos-75exz6derea.ws-eu117.gitpod.io',
 ]
+
+
+# messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success'
+}
