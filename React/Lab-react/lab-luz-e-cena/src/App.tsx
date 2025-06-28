1ded06abbc1c0ed00bfe4b44a674460f08137d70
Jheimys Bassani <jheimysbb@gmail.com>
2025-06-28 18:24:18 +0100

    Creating the banner component

---
+++ React/Lab-react/lab-luz-e-cena/src/App.tsx
@@ -0,0 +1,14 @@
+import Banner from "./components/Banner"
+import bannerDesktop from "./assets/bannerDesktop.png";
+
+
+function App() {
+
+  return (
+    <>
+     <Banner src={bannerDesktop} alt="Banner" />
+    </>
+  )
+}
+
+export default App
