e0418047cb59b10577de164844fc12af8af2204a
Jheimys Bassani <jheimysbb@gmail.com>
2025-09-11 13:08:31 +0100

    Create base_pages

--- Python/selenium-project-pom/pages/login_page.py
+++ Python/selenium-project-pom/pages/login_page.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.common.by import By
 
 import conftest
+from pages.base_pages import BasePage
 
 
-class LoginPage:
+class LoginPage(BasePage):
     def __init__(self):
         self.driver = conftest.driver
         self.username_fild = (By.ID, "user-name")
@@ -11,7 +12,12 @@ class LoginPage:
         self.login_button = (By.ID, 'login-button')
 
     def fazer_login(self, usuario, senha):
-        self.driver.find_element(*self.username_fild).send_keys(usuario)
-        self.driver.find_element(*self.password_fild).send_keys(senha)
-        self.driver.find_element(*self.login_button).click()
+        # self.driver.find_element(*self.username_fild).send_keys(usuario)
+        # self.driver.find_element(*self.password_fild).send_keys(senha)
+        # self.driver.find_element(*self.login_button).click()
+
+        self.escrever(self.username_fild, usuario)
+        self.escrever(self.password_fild, senha)
+        self.clicar(self.login_button)
+
 
